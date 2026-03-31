18550aa04d5932e5b6bf7c22977cec39dca4175d
Moacir-000 <moacir.abreu.943@gmail.com>
2026-03-31 01:05:04 -0300

    princesa-concluida

--- ed/princesa/go/draft.go
+++ ed/princesa/go/draft.go
@@ -1,15 +1,36 @@
 package main
 import "fmt"
 
-func procurar_vivo(elementos []int, size, pos int){
-    fmt.Println(size);
-}
-
 func main() {
     n, e := 0, 0;
     fmt.Scan(&n, &e);
     
     elementos := make([]int, n);
 
-    procurar_vivo(elementos, n, e);
+    for i := range elementos{
+        elementos[i] = i+1;
+    }
+
+    i := e - 1;
+
+    for len(elementos) > 1{
+        fmt.Print("[ ");
+        for j, v := range elementos{
+            if j == i{
+                fmt.Printf("%d> ", v);
+            } else {
+                fmt.Printf("%d ", v);
+            }
+        }
+    
+        fmt.Println("]")
+
+        alvo := (i + 1) % len(elementos)
+
+        elementos = append(elementos[:alvo], elementos[alvo+1:]...)
+
+        i = alvo % len(elementos);
+    }
+
+    fmt.Printf("[ %d> ]\n", elementos[0]);
 }
